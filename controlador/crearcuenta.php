77e62b6a53ffbc609d9e60b995df2848876a92f0
hh <henry.dos@gmail.com>
2018-04-18 23:58:59 -0600

    https://github.com/Ayd1A/Banca-Virtual.git

--- controlador/crearcuenta.php
+++ controlador/crearcuenta.php
@@ -9,6 +9,6 @@ if(isset($_POST['user_type'])){
 }
 	require_once("../modelo/set_cuenta.php");
 	$menu1 = new Cuenta();
-	$pd = $menu1->insertar($tpcuenta,$dpicliente,$saldo);
+	$pd = $menu1->insertar($saldo,$dpicliente,$tpcuenta);
 	header('location:../menuAdmin.php');
 ?>
--- db/bancavirtual.sql
+++ db/bancavirtual.sql
@@ -1,62 +1,56 @@
-	CREATE TABLE `clientes` ( 
-   `idcliente` int(11) NOT NULL AUTO_INCREMENT,
- `nombres` varchar(40) NOT NULL,
-   `apellido` varchar(40) DEFAULT NULL,
- `telefono` varchar(40) DEFAULT NULL,
- `dpi` varchar(40)  NOT NULL,
- `direccion` varchar(40) DEFAULT NULL,
- `correo` varchar(40) NOT NULL,
- `password` varchar(40) NOT NULL,
-  `user_type` varchar(40) NOT NULL,
- PRIMARY KEY (`idcliente`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+CREATE SCHEMA IF NOT EXISTS `bancavirtual` DEFAULT CHARACTER SET utf8 ;
+USE `bancavirtual` ;
+
+-- -----------------------------------------------------
+-- Table `bancavirtual`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bancavirtual`.`cliente` (
+  `dpi` VARCHAR(45) NOT NULL,
+  `nombres` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `telefono` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(45) NOT NULL,
+  `correo` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `user_type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`dpi`))
+ENGINE = InnoDB;
 
 
-INSERT INTO clientes ( nombres,apellido,telefono,dpi,direccion,correo,password,user_type)
-VALUES ('malibu', 'verv', '889', '990', 'Montana', 'malibu@gmail.com','123', 'admin');
-
-CREATE TABLE `cuenta` ( 
-   `idcuenta` int(11) NOT NULL AUTO_INCREMENT,
- `nombre` varchar(40)  DEFAULT NULL,
-   `tipo` varchar(40) NOT NULL, 
- PRIMARY KEY (`idcuenta`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
-INSERT INTO cuenta (nombre, tipo)
-VALUES ('Cuenta Monetaria', 'Monetaria');
-INSERT INTO cuenta (nombre, tipo)
-VALUES ('Cuenta de Ahorro', 'Ahorro');
-
-CREATE TABLE `cliente_cuenta` ( 
-   `idclientcuenta` int(11) NOT NULL AUTO_INCREMENT,
-   `idcliente` int(11) NOT NULL,
-  `idcuenta` int(11) NOT NULL,
-
-   CONSTRAINT FK_client FOREIGN KEY (idcliente)
-    REFERENCES clientes(idcliente)  ON DELETE CASCADE,
-  CONSTRAINT FK_cuenta FOREIGN KEY (idcuenta)
-    REFERENCES cuenta(idcuenta)
-      ON DELETE CASCADE,
- PRIMARY KEY (`idclientcuenta`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
-INSERT INTO cliente_cuenta (idcliente, idcuenta)
-VALUES (1, 2);
+-- -----------------------------------------------------
+-- Table `bancavirtual`.`cuenta`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bancavirtual`.`cuenta` (
+  `idcuenta` INT NOT NULL AUTO_INCREMENT,
+  `saldo` VARCHAR(45) NOT NULL,
+  `cliente_dpi` VARCHAR(45) NOT NULL,
+  `tipo` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idcuenta`, `cliente_dpi`),
+  INDEX `fk_cuenta_cliente_idx` (`cliente_dpi` ASC),
+  CONSTRAINT `fk_cuenta_cliente`
+    FOREIGN KEY (`cliente_dpi`)
+    REFERENCES `bancavirtual`.`cliente` (`dpi`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
-CREATE TABLE `movimientos` ( 
-   `idmov` int(11) NOT NULL AUTO_INCREMENT,
-   `idcliente` int(11) NOT NULL,
-  `idcuenta` int(11) NOT NULL,
- `monto` int(11) NOT NULL,
-  `tipo` varchar(40)  NOT NULL,
-   CONSTRAINT FK_client1 FOREIGN KEY (idcliente)
-    REFERENCES clientes(idcliente)  ON DELETE CASCADE,
-  CONSTRAINT FK_cuenta1 FOREIGN KEY (idcuenta)
-    REFERENCES cuenta(idcuenta)
-      ON DELETE CASCADE,
- PRIMARY KEY (`idmov`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
-INSERT INTO movimientos (idcliente, idcuenta,monto,tipo)
-VALUES (1, 2, 'retiro', '90');
+
+
+-- -----------------------------------------------------
+-- Table `bancavirtual`.`Movimiento`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `bancavirtual`.`Movimiento` (
+  `idMovimiento` INT NOT NULL AUTO_INCREMENT,
+  `monto` VARCHAR(45) NOT NULL,
+  `cuenta_idcuenta` INT NOT NULL,
+  `TipoMovimiento` VARCHAR(45) NULL,
+  PRIMARY KEY (`idMovimiento`, `cuenta_idcuenta`),
+  INDEX `fk_Movimiento_cuenta1_idx` (`cuenta_idcuenta` ASC),
+  CONSTRAINT `fk_Movimiento_cuenta1`
+    FOREIGN KEY (`cuenta_idcuenta`)
+    REFERENCES `bancavirtual`.`cuenta` (`idcuenta`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
