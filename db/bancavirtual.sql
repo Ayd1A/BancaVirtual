e719c8a411758bea5495fdebe72474fd53fdb584
analisis.disenio1@gmail.com <analisis1>
2018-04-18 20:46:57 -0600

    script sql

--- db/bancavirtual.sql
+++ db/bancavirtual.sql
@@ -1,67 +1,229 @@
-	CREATE TABLE `clientes` ( 
-   `idcliente` int(11) NOT NULL AUTO_INCREMENT,
- `nombres` varchar(40) NOT NULL,
-   `apellido` varchar(40) DEFAULT NULL,
- `telefono` varchar(40) DEFAULT NULL,
- `dpi` varchar(40)  NOT NULL,
- `direccion` varchar(40) DEFAULT NULL,
- `correo` varchar(40) NOT NULL,
- `password` varchar(40) NOT NULL,
-  `user_type` varchar(40) NOT NULL,
- PRIMARY KEY (`idcliente`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-04-2018 a las 04:44:34
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-INSERT INTO clientes ( nombres,apellido,telefono,dpi,direccion,correo,password,user_type)
-VALUES ('malibu', 'verv', '889', '990', 'Montana', 'malibu@gmail.com','123', 'admin');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE `cuenta` ( 
-   `idcuenta` int(11) NOT NULL AUTO_INCREMENT,
- `nombre` varchar(40)  DEFAULT NULL,
-   `tipo` varchar(40) NOT NULL, 
- PRIMARY KEY (`idcuenta`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+--
+-- Base de datos: `bancavirtual`
+--
 
-INSERT INTO cuenta (nombre, tipo)
-VALUES ('Cuenta Monetaria', 'Monetaria');
-INSERT INTO cuenta (nombre, tipo)
-VALUES ('Cuenta de Ahorro', 'Ahorro');
+-- --------------------------------------------------------
 
-CREATE TABLE `cliente_cuenta` ( 
-   `idclientcuenta` int(11) NOT NULL AUTO_INCREMENT,
-   `idcliente` int(11) NOT NULL,
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `idcliente` int(11) NOT NULL,
+  `nombres` varchar(40) NOT NULL,
+  `apellido` varchar(40) DEFAULT NULL,
+  `telefono` varchar(40) DEFAULT NULL,
+  `dpi` varchar(40) NOT NULL,
+  `direccion` varchar(40) DEFAULT NULL,
+  `correo` varchar(40) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `user_type` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`idcliente`, `nombres`, `apellido`, `telefono`, `dpi`, `direccion`, `correo`, `password`, `user_type`) VALUES
+(1, 'malibu', 'verv', '889', '990', 'Montana', 'malibu@gmail.com', '123', 'admin'),
+(2, 'mike', 'stere', '4', '3444', 'california', 'mike00@gmail.com', '123', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente_cuenta`
+--
+
+CREATE TABLE `cliente_cuenta` (
+  `idclientcuenta` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
   `idcuenta` int(11) NOT NULL,
- `monto` DECIMAL (10, 2) NOT NULL,
-   CONSTRAINT FK_client FOREIGN KEY (idcliente)
-    REFERENCES clientes(idcliente)  ON DELETE CASCADE,
-  CONSTRAINT FK_cuenta FOREIGN KEY (idcuenta)
-    REFERENCES cuenta(idcuenta)
-      ON DELETE CASCADE,
- PRIMARY KEY (`idclientcuenta`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+  `monto` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO cliente_cuenta (idcliente, idcuenta,monto)
-VALUES (1, 2,200.00);
+--
+-- Volcado de datos para la tabla `cliente_cuenta`
+--
 
-where  idcliente=1
-select nombre from cuenta where  idcuenta=2
+INSERT INTO `cliente_cuenta` (`idclientcuenta`, `idcliente`, `idcuenta`, `monto`) VALUES
+(1, 1, 2, '200.00'),
+(2, 2, 1, '300.00'),
+(4, 1, 1, '1243.00');
 
-select*from cliente_cuenta
-select*from cliente_cuenta where  idcliente=1
+-- --------------------------------------------------------
 
-CREATE TABLE `movimientos` ( 
-   `idmov` int(11) NOT NULL AUTO_INCREMENT,
-   `idcliente` int(11) NOT NULL,
+--
+-- Estructura de tabla para la tabla `cuenta`
+--
+
+CREATE TABLE `cuenta` (
   `idcuenta` int(11) NOT NULL,
- `monto` DECIMAL (10, 2) NOT NULL,
-  `tipo` varchar(40)  NOT NULL,
-   CONSTRAINT FK_client1 FOREIGN KEY (idcliente)
-    REFERENCES clientes(idcliente)  ON DELETE CASCADE,
-  CONSTRAINT FK_cuenta1 FOREIGN KEY (idcuenta)
-    REFERENCES cuenta(idcuenta)
-      ON DELETE CASCADE,
- PRIMARY KEY (`idmov`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+  `nombre` varchar(40) DEFAULT NULL,
+  `tipo` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO movimientos (idcliente, idcuenta,monto,tipo)
-VALUES (1, 2,90.00, 'retiro');
+--
+-- Volcado de datos para la tabla `cuenta`
+--
+
+INSERT INTO `cuenta` (`idcuenta`, `nombre`, `tipo`) VALUES
+(1, 'Cuenta Monetaria', 'Monetaria'),
+(2, 'Cuenta de Ahorro', 'Ahorro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimientos`
+--
+
+CREATE TABLE `movimientos` (
+  `idmov` int(11) NOT NULL,
+  `idcliente` int(11) NOT NULL,
+  `idcuenta` int(11) NOT NULL,
+  `monto` decimal(10,2) NOT NULL,
+  `tipo` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `movimientos`
+--
+
+INSERT INTO `movimientos` (`idmov`, `idcliente`, `idcuenta`, `monto`, `tipo`) VALUES
+(1, 1, 2, '90.00', 'retiro');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL,
+  `usuario` varchar(20) DEFAULT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  `nombre` varchar(30) DEFAULT NULL,
+  `apellidos` varchar(30) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `usuario`, `password`, `nombre`, `apellidos`, `email`) VALUES
+(1, 'luis', '123', 'ffg', 'ggh', 'ddff');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idcliente`);
+
+--
+-- Indices de la tabla `cliente_cuenta`
+--
+ALTER TABLE `cliente_cuenta`
+  ADD PRIMARY KEY (`idclientcuenta`),
+  ADD KEY `FK_client` (`idcliente`),
+  ADD KEY `FK_cuenta` (`idcuenta`);
+
+--
+-- Indices de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`idcuenta`);
+
+--
+-- Indices de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD PRIMARY KEY (`idmov`),
+  ADD KEY `FK_client1` (`idcliente`),
+  ADD KEY `FK_cuenta1` (`idcuenta`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente_cuenta`
+--
+ALTER TABLE `cliente_cuenta`
+  MODIFY `idclientcuenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  MODIFY `idcuenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  MODIFY `idmov` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cliente_cuenta`
+--
+ALTER TABLE `cliente_cuenta`
+  ADD CONSTRAINT `FK_client` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`idcliente`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_cuenta` FOREIGN KEY (`idcuenta`) REFERENCES `cuenta` (`idcuenta`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `movimientos`
+--
+ALTER TABLE `movimientos`
+  ADD CONSTRAINT `FK_client1` FOREIGN KEY (`idcliente`) REFERENCES `clientes` (`idcliente`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_cuenta1` FOREIGN KEY (`idcuenta`) REFERENCES `cuenta` (`idcuenta`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ db/bancavirtualexample.sql
@@ -0,0 +1,67 @@
+	CREATE TABLE `clientes` ( 
+   `idcliente` int(11) NOT NULL AUTO_INCREMENT,
+ `nombres` varchar(40) NOT NULL,
+   `apellido` varchar(40) DEFAULT NULL,
+ `telefono` varchar(40) DEFAULT NULL,
+ `dpi` varchar(40)  NOT NULL,
+ `direccion` varchar(40) DEFAULT NULL,
+ `correo` varchar(40) NOT NULL,
+ `password` varchar(40) NOT NULL,
+  `user_type` varchar(40) NOT NULL,
+ PRIMARY KEY (`idcliente`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+
+INSERT INTO clientes ( nombres,apellido,telefono,dpi,direccion,correo,password,user_type)
+VALUES ('malibu', 'verv', '889', '990', 'Montana', 'malibu@gmail.com','123', 'admin');
+
+CREATE TABLE `cuenta` ( 
+   `idcuenta` int(11) NOT NULL AUTO_INCREMENT,
+ `nombre` varchar(40)  DEFAULT NULL,
+   `tipo` varchar(40) NOT NULL, 
+ PRIMARY KEY (`idcuenta`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+INSERT INTO cuenta (nombre, tipo)
+VALUES ('Cuenta Monetaria', 'Monetaria');
+INSERT INTO cuenta (nombre, tipo)
+VALUES ('Cuenta de Ahorro', 'Ahorro');
+
+CREATE TABLE `cliente_cuenta` ( 
+   `idclientcuenta` int(11) NOT NULL AUTO_INCREMENT,
+   `idcliente` int(11) NOT NULL,
+  `idcuenta` int(11) NOT NULL,
+ `monto` DECIMAL (10, 2) NOT NULL,
+   CONSTRAINT FK_client FOREIGN KEY (idcliente)
+    REFERENCES clientes(idcliente)  ON DELETE CASCADE,
+  CONSTRAINT FK_cuenta FOREIGN KEY (idcuenta)
+    REFERENCES cuenta(idcuenta)
+      ON DELETE CASCADE,
+ PRIMARY KEY (`idclientcuenta`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+INSERT INTO cliente_cuenta (idcliente, idcuenta,monto)
+VALUES (1, 2,200.00);
+
+where  idcliente=1
+select nombre from cuenta where  idcuenta=2
+
+select*from cliente_cuenta
+select*from cliente_cuenta where  idcliente=1
+
+CREATE TABLE `movimientos` ( 
+   `idmov` int(11) NOT NULL AUTO_INCREMENT,
+   `idcliente` int(11) NOT NULL,
+  `idcuenta` int(11) NOT NULL,
+ `monto` DECIMAL (10, 2) NOT NULL,
+  `tipo` varchar(40)  NOT NULL,
+   CONSTRAINT FK_client1 FOREIGN KEY (idcliente)
+    REFERENCES clientes(idcliente)  ON DELETE CASCADE,
+  CONSTRAINT FK_cuenta1 FOREIGN KEY (idcuenta)
+    REFERENCES cuenta(idcuenta)
+      ON DELETE CASCADE,
+ PRIMARY KEY (`idmov`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+INSERT INTO movimientos (idcliente, idcuenta,monto,tipo)
+VALUES (1, 2,90.00, 'retiro');
