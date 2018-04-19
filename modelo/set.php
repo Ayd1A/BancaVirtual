1bd63a33ceab33203165732437a60563e617415a
Ayd1A <37204637+Ayd1A@users.noreply.github.com>
2018-04-19 02:52:30 -0600

    Add files via upload

--- modelo/set.php
+++ modelo/set.php
@@ -7,7 +7,7 @@ private  $errors;
 	public function __construct()
 	{
 		$this->usuarios = array();
-		$this->conexion = new PDO('mysql:host=localhost;dbname=bancavirtual', "root", "");
+		$this->conexion = new PDO('mysql:host=localhost;dbname=ingeniod_myapp', "ingeniod_user", "deseret1234");
 	}
 
 	private function set_names()
@@ -59,6 +59,45 @@ if (count($errors) == 0) {
 	}
 	return 0;
 }
+
+
+
+
+	public function insertarTransTer($nomb,$apell,$tel)
+	{
+	
+		 
+		
+if (empty($nomb)) { 
+		array_push($errors, "Nombre es requerido"); 
+	}
+
+	if (empty($apell)) { 
+		array_push($errors, "Contraseña es requerido"); 
+	}
+	if (empty($tel)) { 
+		array_push($errors, "dpi es requerido"); 
+	}
+	
+	
+if (count($errors) == 0) {
+		//$password_1 = md5($password);//encrypt the password before saving in the database  idcliente 	idcuenta 	monto 	tipo 
+
+		
+	$query = "INSERT INTO movimientos (idcliente, idcuenta , monto, tipo ) 
+					  VALUES('$nomb', '$apell', '$tel', 'debito')";
+			
+			$this->conexion->query($query);
+
+		
+			return 1;
+	}
+	return 0;
+}
+
+
+
+
 	
 	}
 ?>
